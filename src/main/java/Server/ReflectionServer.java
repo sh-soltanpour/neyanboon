10acf0914baa2a647e833ed6dbbd13fb110bd67d
sh-soltanpour <shahryar2006@gmail.com>
2019-02-22 20:50:02 +0330

    add some logs

--- src/main/java/Server/ReflectionServer.java
+++ src/main/java/Server/ReflectionServer.java
@@ -9,12 +9,14 @@ import java.net.InetSocketAddress;
 
 public class ReflectionServer {
     public void startServer() throws Exception {
+        System.out.println("Server is starting...");
         initialize();
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/project", new ProjectController());
         server.createContext("/user", new UserController());
         server.setExecutor(null);
         server.start();
+        System.out.println("Server started");
     }
 
     private void initialize() {
