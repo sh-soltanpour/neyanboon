7df97c9ec6ee63fbac65cebfbb11caa7f7ac8563
sh-soltanpour <shahryar2006@gmail.com>
2019-02-26 00:27:07 +0330

    Add servlet

---
+++ src/main/java/servlets/ProjectServlet.java
@@ -0,0 +1,41 @@
+package servlets;
+
+import Factory.ObjectFactory;
+import dtos.ProjectDto;
+import exceptions.AccessDeniedException;
+import exceptions.NotFoundException;
+import services.project.ProjectService;
+import services.user.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@WebServlet("/project/*")
+public class ProjectServlet extends HttpServlet {
+    private ProjectService projectService = ObjectFactory.getProjectService();
+    private UserService userService = ObjectFactory.getUserService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<String> pathVariables = Arrays.asList(req.getPathInfo().split("/"));
+        String projectId = pathVariables.get(1);
+        try {
+            ProjectDto project = projectService.getProject(userService.getCurrentUser(), projectId);
+            req.setAttribute("project", project);
+            req.getRequestDispatcher("/project.jsp").forward(req, resp);
+
+        } catch (AccessDeniedException e) {
+            System.out.println("exception denied");
+
+        } catch (NotFoundException e) {
+            System.out.println("exception not found");
+        }
+
+    }
+}
