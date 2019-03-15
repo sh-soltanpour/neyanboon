4b999bc2ee6f7b36049cf6bb1ef9457c9b4eff77
sh-soltanpour <shahryar2006@gmail.com>
2019-03-15 14:33:37 +0330

    remove useless imports

--- src/main/java/servlets/SkillServlet.java
+++ src/main/java/servlets/SkillServlet.java
@@ -1,17 +1,13 @@
 package servlets;
 
-import dtos.ProjectSkillDto;
-import exceptions.NotFoundException;
 import factory.ObjectFactory;
 import services.skill.SkillService;
-import services.user.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.stream.Collectors;
 
 @WebServlet("/skills")
 public class SkillServlet extends BaseServlet {
