5d29d0f2c737892390d7d6ebec7e6854514c551e
sh-soltanpour <shahryar2006@gmail.com>
2019-03-15 13:08:44 +0330

    Add cors filter

---
+++ src/main/java/configuration/CorsFilter.java
@@ -0,0 +1,16 @@
+package configuration;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        chain.doFilter(request, response);
+    }
+}
