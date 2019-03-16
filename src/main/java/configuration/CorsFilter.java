fca1fb2332388b09ee757f512bae4268615964ad
sh-soltanpour <shahryar2006@gmail.com>
2019-03-16 19:28:07 +0330

    improve corsFilter

--- src/main/java/configuration/CorsFilter.java
+++ src/main/java/configuration/CorsFilter.java
@@ -2,6 +2,7 @@ package configuration;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -9,8 +10,12 @@ import java.io.IOException;
 public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         httpResponse.setHeader("Access-Control-Allow-Origin", "*");
-        chain.doFilter(request, response);
+        httpResponse.setHeader("Access-Control-Allow-Headers", "content-type, accept, origin, referer");
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        if (!httpRequest.getMethod().equals("OPTIONS"))
+            chain.doFilter(request, response);
     }
 }
