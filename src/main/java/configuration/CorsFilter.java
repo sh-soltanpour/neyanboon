7366cb1bb85e336ecdcb2794b84615e13c21aded
sh-soltanpour <shahryar2006@gmail.com>
2019-05-17 21:01:49 +0430

    add x-auth-token to allowed headers

--- src/main/java/configuration/CorsFilter.java
+++ src/main/java/configuration/CorsFilter.java
@@ -6,14 +6,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(filterName ="CorsFilter")
+@WebFilter(filterName = "CorsFilter")
 public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         httpResponse.setHeader("Access-Control-Allow-Origin", "*");
-        httpResponse.setHeader("Access-Control-Allow-Headers", "content-type, accept, origin, referer");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "x-auth-token, content-type, accept, origin, referer");
         httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
         httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         if (!httpRequest.getMethod().equals("OPTIONS"))
